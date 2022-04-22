d9bd3a0eea1cf582538bada411308587701a2b64
ud94 <806613711@qq.com>
2022-04-22 19:38:15 +0800

    gpl

--- Function/html.go
+++ Function/html.go
@@ -8,6 +8,8 @@ import (
 	"html/template"
 	"net/http"
 	"time"
+
+	"github.com/UD94/SecondOP/Common"
 )
 
 type Line struct {
@@ -16,6 +18,13 @@ type Line struct {
 	SP int     `json:"sp"`
 	AG int     `json:"ag"`
 	TM int     `json:"tm"`
+}
+
+type Androidinfo struct {
+	Line    string
+	Job     string
+	Message string
+	Reverso string
 }
 
 func Linequery(DB *sql.DB, w http.ResponseWriter, name string, starttime string, endtime string) (string, error) {
@@ -48,13 +57,40 @@ func Linequery(DB *sql.DB, w http.ResponseWriter, name string, starttime string,
 
 }
 
-func Lineinsert(DB *sql.DB, X string, Y string, AG string, name string) (string, error) {
+func Androidjob(DB *sql.DB, target string, value string) (string, error) {
+	_, err := DB.Exec("UPDATE androids.target SET line= ?, job= ?  WHERE (line= ? )  LIMIT 1;	", target, value, target)
+	if err != nil {
+		fmt.Println("新增数据错误", err)
 
-	_, err := DB.Exec("insert into androids.record(X,Y,SP,AG,TM,line) values(?,?,10,?,?,?)", X, Y, AG, time.Now().Unix(), name)
+		return "updateerror", errors.New("update error")
+	}
+	return "success", nil
+}
+
+func Androidjobqeury(DB *sql.DB, target string) (string, error) {
+	var user Androidinfo
+	err := DB.QueryRow("select * from target where line = ? ", target).Scan(&user.Line, &user.Job, &user.Message, &user.Reverso)
+	if err != nil {
+		fmt.Println("查询出错了", err)
+
+		return "nopass", errors.New("no pass")
+	}
+
+	return user.Job, nil
+}
+
+func Lineinsert(DB *sql.DB, X string, Y string, AG string, name string, Useragent string) (string, error) {
+
+	_, err := DB.Exec("insert into androids.record(X,Y,SP,AG,TM,line,UA) values(?,?,10,?,?,?,?)", X, Y, AG, time.Now().Unix(), name, Useragent)
 	if err != nil {
 		fmt.Println("新增数据错误", err)
 
 		return "inserterror", errors.New("insert error")
+	}
+	lists, _ := Linelist(DB)
+	if Common.In(name, lists) {
+		DB.Exec("insert into androids.target(line,job,message,Reverso) values(?,?,?,?)", name, "0", "empty", "empty")
+
 	}
 
 	return "success", nil
